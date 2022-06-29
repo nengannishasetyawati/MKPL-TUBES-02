ee1350cd352e266181f066fcc0c5a7967b86eb07
ryanfahrez <55272787+ryanfahrez@users.noreply.github.com>
2022-06-29 16:21:43 +0700

    Update GithubActionsExampleApplication.java

--- src/main/java/com/techprimers/githubactionsexample/GithubActionsExampleApplication.java
+++ src/main/java/com/techprimers/githubactionsexample/GithubActionsExampleApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class GithubActionsExampleApplication {
+public class GithubActionsExampleApplicationn {
 
 	public static void main(String[] args) {
-		SpringApplication.run(GithubActionsExampleApplication.class, args);
+		SpringApplication.run(GithubActionsExampleApplicationnnn.class, args);
 	}
 
 }
